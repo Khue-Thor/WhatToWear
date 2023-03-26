1b894b377f9200ccb09148abbb2ff35f090a1535
khuephamy <khuephumy19@gmail.com>
2023-03-26 15:06:04 -0500

    images/constants.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,2 +1,13 @@
 import React from "react";
-import "./Header.css"
+import "./Header.css";
+import logoPath from "../../images/wtwr.svg";
+import avatarPath from "../../images/avatar.svg";
+import { currentDate } from "../../utils/constants";
+
+export function Header() {
+  return (
+    <div className="header">
+
+    </div>
+  )
+}
