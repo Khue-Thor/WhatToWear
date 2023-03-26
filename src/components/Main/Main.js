d925162175a7f76b57f4abd4511a48acfc35f0e5
khuephamy <khuephumy19@gmail.com>
2023-03-26 18:03:22 -0500

    update Main temperature toggle

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,11 @@
-import React from "react";
+import { React, useContext } from "react";
 import "./Main.css";
 import { ItemCard } from "../ItemCard/ItemCard";
 import { WeatherCard } from "../WeatherCard/WeatherCard";
-
+import { CurrentTemperatureUnitContext } from "../../context/CurrentTemperatureUnitContext";
 export function Main({ weatherData, cards, onCardClick }) {
   
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
   const temperature = weatherData.main?.temp;
   const getWeatherType = () => {
     if (temperature >= 86) {
@@ -19,14 +20,21 @@ export function Main({ weatherData, cards, onCardClick }) {
   const filteredCards = cards.filter((card) => card.weather === weatherType);
   return (
     <main className="main">
-      <WeatherCard weatherData={weatherData}/>
+      <WeatherCard weatherData={weatherData} />
       <section className="main__clothes-container">
         <div className="main__info">
           <div className="main__description-container">
-            <p className="main__description">
-              Today is {Math.round(temperature)}°F and it is {weatherType} / You may want to
-              wear:
-            </p>
+            {currentTemperatureUnit === "F" ? (
+              <p className="main__description">
+                Today is {Math.round(temperature)}°{currentTemperatureUnit} and it is {weatherType}{" "}
+                / You may want to wear:
+              </p>
+            ) : (
+              <p className="main__description">
+                Today is {Math.round(((temperature - 32) * 5) / 9)}°{currentTemperatureUnit} and it
+                is {weatherType} / You may want to wear:
+              </p>
+            )}
           </div>
         </div>
         <ul className="main__items">
@@ -36,5 +44,5 @@ export function Main({ weatherData, cards, onCardClick }) {
         </ul>
       </section>
     </main>
-  )
+  );
 }
