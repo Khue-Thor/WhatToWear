c5e6cb951e7f8a85c2fc3cd6747ca75cfa65bce1
khuephamy <khuephumy19@gmail.com>
2023-03-26 19:43:39 -0500

    create ModalForm/AdditemModal

---
+++ src/components/AddItemModal/AddItemModal.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+import { ModalWithForm } from "../ModalWithForm/ModalWithForm";
+export const AddItemModal = ({ name, isOpen, isLoading, onAddItem, onCloseModal }) => {
+  
+}
