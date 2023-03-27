aeb4a9e18c186a8e92b2ea041b14f11fb607d087
khuephamy <khuephumy19@gmail.com>
2023-03-27 09:33:44 -0500

    Profile/ClothesSection feature

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Profile.css";
+import { ClothesSection } from "../ClothesSection/ClothesSection";
+import { SideBar } from "../SideBar/SideBar";
+
+export function Profile({ cards, handleAddClick, onCardClick }) {
+  
+  return (
+    <div className="profile">
+      <SideBar />
+      <ClothesSection cards={cards} handleAddClick={handleAddClick} onCardClick={onCardClick} />
+    </div>
+  );
+}
