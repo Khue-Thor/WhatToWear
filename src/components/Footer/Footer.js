be766207ce8e5101993fa7d33fa5e7f2b9897d37
khuephamy <khuephumy19@gmail.com>
2023-03-30 19:02:38 -0500

    style footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './Footer.css';
-
+import { date, currentDate } from "../../utils/constants";
+import { CurrentTemperatureUnitContext } from "../../context/CurrentTemperatureUnitContext";
 export function Footer() {
 
   return (
     <div className="footer">
       <div className="footer__container">
-        
+        <p className="footer__description">© Developed by Khuephamy Phialouang - {date}</p>
+        <div className="footer__social-container">
+
+        </div>
       </div>
     </div>
   )
